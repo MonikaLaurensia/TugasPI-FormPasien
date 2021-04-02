4601de9055e3f4f812c749b5eaa6819d2776ba57
patrisiatbn <patrisiatbn@gmail.com>
2021-04-03 00:49:47 +0700

    Mempercantik tampilan tabel data pasien dengan menambahkan class css pada tampilTabel.php

--- tampilTabel.php
+++ tampilTabel.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
+<div class="container-fluid" >
 <head>
   <meta charset="utf-8">
-  <title>FORM TAMBAH PASIEN</title>
+  <title>Data Pasien</title>
   <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
-<body>
+<body class="bg-gradient-info">
   <div class="header">
     <div class="header-left">Sistem Informasi Klinik</div>
     <div class="header-right">
@@ -16,13 +17,13 @@
       </ul>
     </div>
   </div>
+<div class="container">
+<h1 class="title">Daftar Pasien </h1>
 
-<h1>Daftar Pasien </h1>
+<a href="index.php"><button type="button" class=" btn btn-success mt-3">Tambahkan Data Pasien</button></a>
 
-<a href="index.php">Tambah data pasien </a>
-
-<table border="1" cellspacing="0">
-    <tr>
+<table class=" text-center table table-hover mt-4" style="background-color: white" align="center">
+    <tr class="table" style="background-color:#40E0D0" >
         <th> No. </th>
         <th> Aksi </th>
         <th> No Rekam Medis </th>
@@ -39,22 +40,23 @@
     <tr>
         <td> <?= $i; ?> </td>
         <td>
-            <a href="ubah.php?id=<?= $psn["id"]; ?>">edit</a> |
-            <a href="hapus.php?id=<?= $psn["id"]; ?>">hapus</a>
+            <a href="ubah1.php?id=<?= $psn["id"]; ?>"><button type="submit" class="btn btn-info">Edit</button></a>
+            <a href="hapus.php?id=<?= $psn["id"]; ?>"><button type="submit" class="btn btn-danger">Hapus</button></a>
         </td>
         <td><?= $psn["no_rk"]; ?></td>
         <td><?= $psn["name"]; ?></td>
         <td><?= $psn["gender"]; ?></td>
-        <td><?= $psn["age"]; ?></umur>
+        <td><?= $psn["age"]; ?></td>
         <td><?= $psn["no_hp"]; ?></td>
-        <td><?= $psn["address"]; ?>/td>
         <td><?= $psn["poli"]; ?></td>
+        <td><?= $psn["address"]; ?></td>
     </tr>
     
     <?php $i++; ?>
     <?php endforeach; ?>
 </table>
 
-    
+</div>
+</div>
 </body>
 </html>
